65550dd2f99ab42b38159b0815192543c88ef130
Learner <vivek.gurukul1982@gmail.com>
2019-06-21 17:24:41 -0700

    Create container-with-most-water.java

---
+++ container-with-most-water.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/container-with-most-water/
+class container-with-most-water {
+    // tc -> n, sc-> 1
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length-1;
+        int maxArea = 0;
+        while(left < right){
+            maxArea = Math.max(maxArea, 
+                               Math.min(height[left], height[right])*(right-left));
+            if(height[left]<height[right]){
+                left++;
+            }else{
+                right--;
+            }
+         }
+        return maxArea;
+    }
+}
