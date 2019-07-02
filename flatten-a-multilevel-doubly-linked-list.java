e466d1f7ab1f0eb2b712b675b3605fc08c7f1127
Learner <vivek.gurukul1982@gmail.com>
2019-07-02 10:25:28 -0700

    Create flatten-a-multilevel-doubly-linked-list.java

---
+++ flatten-a-multilevel-doubly-linked-list.java
@@ -0,0 +1,46 @@
+// https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list/
+/*
+Pattern - Use stack to keep a track of next when child got found
+need to handle only 2 conditions:
+1. When child is not null
+2. when curr next is null means we reach to the tail of curr node through child node
+*/
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+
+    public Node() {}
+
+    public Node(int _val,Node _prev,Node _next,Node _child) {
+        val = _val;
+        prev = _prev;
+        next = _next;
+        child = _child;
+    }
+};
+*/
+class Solution {
+    // tc -> n, sc-> n
+    public Node flatten(Node head) {
+        Node curr = head;
+        Stack<Node> stack = new Stack<>();
+        
+        while(curr!=null){
+            if(curr.child!=null){
+                stack.push(curr.next);
+                curr.next = curr.child;
+                if(curr.next!=null) curr.next.prev = curr;
+                curr.child = null; // Imp
+            }else if(curr.next==null && !stack.isEmpty()){
+                curr.next = stack.pop();
+                if(curr.next!=null) curr.next.prev = curr;
+            }
+            curr = curr.next;
+        }        
+        return head;
+    }
+}
