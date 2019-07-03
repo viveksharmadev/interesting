1c88fdea61cec5bd88f4c10b64e4ec00cac1df35
Learner <vivek.gurukul1982@gmail.com>
2019-07-03 16:12:40 -0700

    Update single-element-in-a-sorted-array.java

--- single-element-in-a-sorted-array.java
+++ single-element-in-a-sorted-array.java
@@ -15,4 +15,28 @@ class single-element-in-a-sorted-array {
         
         return -1;
     }
+    
+    // Optimized
+    // tc -> logn, sc-> 1
+    public int singleNonDuplicate(int[] nums) {
+        int start = 0, end = nums.length-1;
+        
+        while(start < end){
+            int mid = start + (end-start)/2;
+            if(mid%2==0){
+                if(nums[mid]==nums[mid+1]){
+                    start = mid+2;
+                }else{
+                    end = mid;
+                }
+            }else{
+                 if(nums[mid]==nums[mid-1]){
+                    start = mid+1;
+                }else{
+                    end = mid;
+                }
+            }
+        }
+        return nums[start];
+    }
 }
