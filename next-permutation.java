cf3689edbb36e4d77e70b04d63986d7685e0952f
Learner <vivek.gurukul1982@gmail.com>
2019-07-10 12:31:01 -0700

    Update next-permutation.java

--- next-permutation.java
+++ next-permutation.java
@@ -1,14 +1,19 @@
-   // https://leetcode.com/problems/next-permutation/
-   
-     // Step 1 first get the culprit means the digit that is lesser than any neighbor next digit because this is the candidate to swap with and get bigger number, It is easier to check from last to first to get the immediate smaller digit (culprit) to swap further in step3.
-        // From Step1 : Quesion arise why we are not swapping right away in step1 and going to step3 to swap? The answer is because we want to swap with the very last digit in hope to get immediate next greater number so we have to restart checking again from last digit in array to swap with candidate digit accordingly. 
-        // Step2 let's consider we did not find such digit means the given number is in descending order. Go ahead and reverse the number and return, we are done for such input
-        // Step 3: if control reaches to step 3 means we have to swap the potential candidate that we found in step1, now swap this potential candidate number with the last digit
-        // Step 4: this is important step because keep in mind that we need to build the next immediate permutation so sort the remaining digits in ascending order after candidate index
-        // Step5 : We are finally DONE.. Congrats
+/* // Step 1: first get the culprit means the digit that is lesser than any bigger digit from    
+		  last because this is the candidate to swap with the next bigger digit, in hope 
+		  to get next bigger number, It is easier to check from last to first to get the immediate               
+		  smaller digit (culprit) to swap further in step3.
+     // From Step1 : Question arise why we are not swapping right away in step1 and going to step3 to swap? 
+		The answer is because we want to swap with the next bigger digit from last in hope to get immediate next 
+		greater number so we have to restart checking again from  next bigger digit from last in array to swap with candidate digit accordingly. 
+     // Step2: let's consider we did not find such digit means the given number is in descending order. 
+	      Go ahead and reverse the number and return, we are done for such input
+     // Step 3: if control reaches to step 3 means we have to swap the potential candidate that we found in step1, 
+		now swap this potential candidate number with the last digit
+     // Step 4: this is important step because keep in mind that we need to build the next immediate permutation so sort the 
+		remaining digits in ascending order after candidate index
+     // Step5 : We are finally DONE.. Congrats */
 
-// tc -> n, sc-> 1
-class next-permutation {
+class Solution {
     public void nextPermutation(int[] nums) {    
         
         int n = nums.length, candidate = 0, candidateIndex = -1;
