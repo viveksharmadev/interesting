30697350c9ce4460e877732e77e0c809b00d3572
Learner <vivek.gurukul1982@gmail.com>
2019-07-10 11:59:47 -0700

    Update next-permutation.java

--- next-permutation.java
+++ next-permutation.java
@@ -1,6 +1,6 @@
    // https://leetcode.com/problems/next-permutation/
    
-         // Step 1 first get the culprit means the digit that is lesser than any neighbor next digit because this is the candidate to swap with and get bigger number, It is easier to check from last to first to get the immediate smaller digit (culprit) to swap further in step3.
+     // Step 1 first get the culprit means the digit that is lesser than any neighbor next digit because this is the candidate to swap with and get bigger number, It is easier to check from last to first to get the immediate smaller digit (culprit) to swap further in step3.
         // From Step1 : Quesion arise why we are not swapping right away in step1 and going to step3 to swap? The answer is because we want to swap with the very last digit in hope to get immediate next greater number so we have to restart checking again from last digit in array to swap with candidate digit accordingly. 
         // Step2 let's consider we did not find such digit means the given number is in descending order. Go ahead and reverse the number and return, we are done for such input
         // Step 3: if control reaches to step 3 means we have to swap the potential candidate that we found in step1, now swap this potential candidate number with the last digit
@@ -23,7 +23,7 @@ class next-permutation {
         
         // Step2:
         if(candidateIndex==-1){
-            sort(nums, 0, n-1);
+            reverse(nums, 0, n-1);
             return;
         }
         
@@ -36,10 +36,10 @@ class next-permutation {
         }
         
         //Step4:
-        sort(nums, candidateIndex+1, n-1);
+        reverse(nums, candidateIndex+1, n-1);
     }
     
-    private void sort(int[] nums, int start, int end){
+    private void reverse(int[] nums, int start, int end){
         while(start < end){
             swap(nums, start, end);
             start++; end--;
