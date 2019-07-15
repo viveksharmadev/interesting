65fce7e8760a21cba5d08994ee9a6eb4e214e4ce
Learner <vivek.gurukul1982@gmail.com>
2019-07-15 10:43:31 -0700

    Update single-element-in-a-sorted-array.java

--- single-element-in-a-sorted-array.java
+++ single-element-in-a-sorted-array.java
@@ -38,5 +38,33 @@ class single-element-in-a-sorted-array {
             }
         }
         return nums[start];
+    }   
+    
+ 
+    /*
+    Intution - we need to acheive logn complexity, it means we may find mid 
+    now once we find mid, we can check its neighbor to understand next level of intution
+    lets say mid is odd means before that, subarray length is even, in this case we can compare 
+    nums at mid with nums at mid-1, if they are not equal then there is a high chance that number
+    at mid is output, then limit our search say end = mid, let start be unchanged means still 0. again calculate mid in loop, this time mid again should be odd, and numbers at mid and mid-1 should equal so say start = mid+2, because if 2 numbers are equal then the next culprit would be currIndex+2 only. 
+    */
+    // tc - > logn, sc-> 1
+    public int singleNonDuplicate(int[] nums) {
+        int start = 0, end = nums.length-1;        
+        while(start < end){
+            int mid = start + (end-start)/2;
+            
+            if(mid%2==1) mid = mid-1;
+            
+            if(nums[mid]!=nums[mid+1]){
+                end = mid;
+            }else{
+                start = mid+2;
+            }
+        }
+        return nums[start];
     }
 }
+
+
+
