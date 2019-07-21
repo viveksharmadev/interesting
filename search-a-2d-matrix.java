5b7fb52b89ed8058750bf76a434bcaf37e49bbc9
Learner <vivek.gurukul1982@gmail.com>
2019-07-21 07:35:46 -0700

    Create search-a-2d-matrix.java

---
+++ search-a-2d-matrix.java
@@ -0,0 +1,41 @@
+// https://leetcode.com/problems/search-a-2d-matrix/
+class search-a-2d-matrix {
+    //tc-> logmn, sc-> 1
+    public boolean searchMatrix(int[][] matrix, int target) {
+        for(int[] m : matrix){
+            if(doesExist(m, target)) return true;
+        }
+        return false;
+    }
+    
+    private boolean doesExist(int[] matrix, int target){
+        int start = 0, end = matrix.length-1;
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            if(matrix[mid]==target) return true;
+            if(target<matrix[mid]) end = mid-1;
+            else start = mid+1;
+        }
+        return false;
+    }
+    
+    //tc-> logmn, sc-> 1
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix==null || matrix.length==0) return false;
+        int m = matrix.length, n = matrix[0].length;
+        
+        int start = 0, end = m*n-1;
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            int pivot = matrix[mid/n][mid%n];
+            if(pivot==target){
+                return true;
+            }
+            if(target<pivot)
+                end = mid-1;
+            else
+                start = mid+1;
+        }
+        return false;
+    }
+}
