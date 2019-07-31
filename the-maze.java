6fd1e176099ae319752bb8981e832d5817721a5e
Learner <vivek.gurukul1982@gmail.com>
2019-07-31 16:36:46 -0700

    Update the-maze.java

--- the-maze.java
+++ the-maze.java
@@ -1,40 +1,35 @@
 // https://leetcode.com/problems/the-maze/
 
 class the-maze {
-    // tc -> m*n ,sc-> m*n
     public boolean hasPath(int[][] maze, int[] start, int[] destination) {
+        // Ball will not stop -> DFS
+        // After hitting the wall, ball can go to any of four directions -> BFS
         Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{start[0], start[1]});
+        q.offer(start);
         boolean[][] visited = new boolean[maze.length][maze[0].length];
+        visited[start[0]][start[1]] = true;
         int[][] dirs = {{0,1},{0,-1},{1,0},{-1,0}};
-        
         while(!q.isEmpty()){
-            int size = q.size();            
-            for(int i=0; i<size; i++){
-                int[] curr = q.poll();
-                int x = curr[0];
+            int[] curr = q.poll();
+            if(curr[0]==destination[0] && curr[1]==destination[1]) return true;
+            for(int[] dir : dirs){
+                int x= curr[0];
                 int y = curr[1];
-                if(x==destination[0] && y==destination[1]) return true;
-                for(int[] dir : dirs){
-                    int xx = x;
-                    int yy = y;
-                    while(isValid(xx+dir[0], yy+dir[1], maze)){
-                        xx+=dir[0];
-                        yy+=dir[1];
-                    }
-                    if(!visited[xx][yy]){
-                    q.offer(new int[]{xx, yy});
-                    visited[xx][yy] = true;
-                }
+                while(isValid(x+dir[0], y+dir[1], maze)){
+                    x += dir[0];
+                    y += dir[1];
                 }
                 
-            }                        
+                if(!visited[x][y]){
+                    visited[x][y] = true;
+                    q.offer(new int[]{x,y});
+                }
+            }            
         }
         return false;
     }
     
     private boolean isValid(int x, int y, int[][] maze){
-        return x>=0 && x<maze.length && y>=0 && y<maze[0].length 
-            && maze[x][y]!=1;
+        return x>=0 && x<maze.length && y>=0 && y<maze[0].length && maze[x][y]!=1;
     }
 }
