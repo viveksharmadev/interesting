c8f4e6f28661a0b496613cbc140e204e74a2c9cc
Learner <vivek.gurukul1982@gmail.com>
2019-08-08 08:01:45 -0700

    Update subtree-of-another-tree.java

--- subtree-of-another-tree.java
+++ subtree-of-another-tree.java
@@ -18,6 +18,20 @@ class subtree-of-another-tree {
         return isSubtree(s.left, t) || isSubtree(s.right, t);
     }
     
+    // Using BFS
+     // tc -> m*n , sc->  n (The depth of the recursion tree can go upto n. n refers to the number of nodes in s)
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(s);
+        while(!q.isEmpty()){
+            TreeNode node = q.poll();
+            if(isSametree(node, t)) return true;
+            if(node.left!=null) q.offer(node.left);
+            if(node.right!=null) q.offer(node.right);
+        }
+        return false;
+    }
+    
     private boolean isSametree(TreeNode s, TreeNode t){
         if(s==null && t==null) return true;
         if(s==null || t==null) return false;
