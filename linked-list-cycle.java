ad9599b3c54ddfec0a64fcd4cd1049ea06f9eef6
Learner <vivek.gurukul1982@gmail.com>
2019-09-19 08:09:41 -0700

    Create linked-list-cycle.java

---
+++ linked-list-cycle.java
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/linked-list-cycle/
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class linked-list-cycle {
+    // tc -> n, sc-> 1
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow==fast) return true;
+        }
+        return false;
+    }
+}
