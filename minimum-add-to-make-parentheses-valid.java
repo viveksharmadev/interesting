1f5d68d4f16f6145fdd539d83935b2a16638c19e
Learner <vivek.gurukul1982@gmail.com>
2019-09-26 05:55:06 -0700

    Create minimum-add-to-make-parentheses-valid.java

---
+++ minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,44 @@
+//https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/
+
+class minimum-add-to-make-parentheses-valid {
+    // tc -> n, sc-> n
+    public int minAddToMakeValid(String S) {
+        Stack<Character> stack = new Stack<>();
+        int count = 0;
+        for(int i=0; i<S.length(); i++){
+            if(S.charAt(i)=='('){
+                stack.push(')');
+                count++;
+            }else if(stack.isEmpty() || stack.peek()!=S.charAt(i)){
+                if(!stack.isEmpty()) stack.pop();
+                count++;
+            }else if(stack.isEmpty() || stack.peek()==S.charAt(i)){
+                if(!stack.isEmpty()) stack.pop();
+                count--;
+            }
+        }
+        return count;
+    }
+    // tc -> n, sc -> n
+    public int minAddToMakeValid(String S) {
+        Deque<Character> stk = new ArrayDeque<>();
+        int count = 0;
+        for (char c : S.toCharArray()) {
+            if (c == '(') { stk.push(c); }
+            else if (stk.isEmpty()) { ++count; }
+            else { stk.pop(); }
+        }
+        return count + stk.size();
+    }
+    
+    // tc -> n, sc -> 1
+    public int minAddToMakeValid(String S) {
+        int count = 0, stk = 0;
+        for (int i = 0; i < S.length(); ++i) {
+            if (S.charAt(i) == '(') { ++stk; }
+            else if (stk <= 0) { ++count; }
+            else { --stk; }
+        }
+        return count + stk;
+    }
+}
