9e0a524cae1a51d3cd482adf09056687fefcaafb
Learner <vivek.gurukul1982@gmail.com>
2019-10-03 05:34:03 -0700

    Create print-in-order.java

---
+++ print-in-order.java
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/print-in-order/
+
+import java.util.concurrent.*;
+class Foo {
+
+    Semaphore run2;
+    Semaphore run3;
+    
+    public Foo() {
+        run2 = new Semaphore(0);
+        run3 = new Semaphore(0);
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        run2.release();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        run2.acquire();
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+        run2.release();
+        run3.release();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        run3.acquire();
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();    
+        run3.release();
+    }
+}
