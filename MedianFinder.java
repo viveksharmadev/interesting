81b8adc1f4a57f5afcb9efcd0b0145a5fc93cb37
Learner <vivek.gurukul1982@gmail.com>
2019-10-17 09:50:37 -0700

    Update MedianFinder.java

--- MedianFinder.java
+++ MedianFinder.java
@@ -39,3 +39,39 @@ class MedianFinder {
  * obj.addNum(num);
  * double param_2 = obj.findMedian();
  */
+
+// Other way, same approach
+
+class MedianFinder {
+
+    // tc -> nlogn, sc-> n
+    PriorityQueue<Integer> lower;
+    PriorityQueue<Integer> higher;
+    
+    /** initialize your data structure here. */
+    public MedianFinder() {
+        lower = new PriorityQueue<>((a,b)->b-a);
+        higher = new PriorityQueue<>();
+    }
+    
+    public void addNum(int num) {
+        lower.offer(num);
+        higher.offer(lower.poll());
+        if(lower.size()<higher.size()){
+            lower.offer(higher.poll());
+        }
+    }
+    
+    public double findMedian() {
+        return lower.size()==higher.size()
+            ? (lower.peek() + higher.peek())/2.0 
+            : lower.peek();
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
