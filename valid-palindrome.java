8ea0aa645781bdd33c117b0301dec85e8695e86e
Learner <vivek.gurukul1982@gmail.com>
2019-10-22 06:14:05 -0700

    Update valid-palindrome.java

--- valid-palindrome.java
+++ valid-palindrome.java
@@ -17,4 +17,24 @@ class valid-palindrome {
         return rev.toString().toLowerCase()
             .equals(sb.reverse().toString().toLowerCase());
     }
+    
+    // tc -> n, sc-> 1
+    public boolean isPalindrome(String s) {
+        int start = 0, end = s.length()-1;
+        s = s.toLowerCase();
+        while(start < end){
+            if(!Character.isLetterOrDigit(s.charAt(start))){
+                start++;
+            }else if(!Character.isLetterOrDigit(s.charAt(end))){
+                end--;
+            }else{
+                if(s.charAt(start)!=s.charAt(end)){
+                    return false;
+                }else{
+                    start++; end--;
+                }
+            }
+        }
+        return true;
+    }
 }
