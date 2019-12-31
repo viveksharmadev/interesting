ee3c1716b7f49b3958501955f7ab1b323e666b46
Learner <vivek.gurukul1982@gmail.com>
2019-12-31 15:44:25 -0800

    Update remove-all-adjacent-duplicates-in-string.java

--- remove-all-adjacent-duplicates-in-string.java
+++ remove-all-adjacent-duplicates-in-string.java
@@ -1,6 +1,6 @@
 //  https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
 class remove-all-adjacent-duplicates-in-string {
-    // tc -> n, sc-> n
+    // tc -> n, sc-> n-d, where d is the number of duplicate chars
     public String removeDuplicates(String S) {
         Stack<Character> stack = new Stack<>();
         for(int i=0; i<S.length(); i++){
@@ -12,7 +12,7 @@ class remove-all-adjacent-duplicates-in-string {
         }
         
         StringBuilder sb = new StringBuilder();
-        for(char c : stack) sb.append(c); // it is taking from stack in FIFO order, interesting        
+        for(char c : stack) sb.append(c); // it is taking from stack in insertion order, interesting        
         return sb.toString();
     }
 }
