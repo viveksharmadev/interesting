924b5196a0b47ad1533d2a8d095f01a2c048f016
Learner <vivek.gurukul1982@gmail.com>
2020-05-15 20:51:30 -0700

    Update find-the-town-judge.java

--- find-the-town-judge.java
+++ find-the-town-judge.java
@@ -1,39 +1,22 @@
 // https://leetcode.com/problems/find-the-town-judge/
 
 class find-the-town-judge {
-    // tc -> T+N , sc-> N
+    // tc -> E, sc-> V
     public int findJudge(int N, int[][] trust) {
-        int[] trusting = new int[N];
-        int[] trusted = new int[N];
+        int[] inArrows = new int[N+1];
+        int[] outArrows = new int[N+1];
         
         for(int[] t : trust){
-            trusting[t[0]-1]++;
-            trusted[t[1]-1]++;
+            outArrows[t[0]]++;
+            inArrows[t[1]]++;
         }
         
-        for(int i=0; i<N; i++){
-            if(trusting[i]==0 && trusted[i]==N-1)
-                return i+1;
+        for(int i=1; i<=N; i++){
+            if(inArrows[i]==N-1 && outArrows[i]==0){
+                return i;
+            }
         }
         
         return -1;
-    }
-    
-    // Compact code
-    // tc -> T+N , sc-> N
-    public int findJudge(int N, int[][] trust) {
-        int[] trusted = new int[N];
-        
-        for(int[] t : trust){
-            trusted[t[0]-1]--;
-            trusted[t[1]-1]++;
-        }
-        
-        for(int i=0; i<N; i++){
-            if(trusted[i]==N-1)
-                return i+1;
-        }
-        
-        return -1;
-    }
+     }
 }
